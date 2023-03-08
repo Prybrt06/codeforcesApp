a392430c1ef1d333be00df0a97ff7c95959fd275
Priyabrat  Duarah <priyabrat@MacBook-Air.local>
2023-03-09 03:42:26 +0530

    More filtering option added in contest list

--- lib/Screens/ContestListScreen.dart
+++ lib/Screens/ContestListScreen.dart
@@ -3,7 +3,6 @@ import 'package:codeforces_help/provider/listProvider.dart';
 import 'package:codeforces_help/utils/storageHandler.dart';
 import 'package:codeforces_help/widget/contestWidget.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 class ContestListScreen extends StatefulWidget {
@@ -14,13 +13,17 @@ class ContestListScreen extends StatefulWidget {
 }
 
 class _ContestListScreenState extends State<ContestListScreen> {
-  List<String> itemList = ["All", "Upcoming", "Past"];
-  String dropDownValue = "All";
+  List<String> itemList = ["Today", "This Week", "All", "Upcoming", "Past"];
+  String dropDownValue = "Today";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Contest List"),
+        title: Row(
+          children: const [
+            Text("Contest List"),
+          ],
+        ),
         backgroundColor: Theme.of(context).primaryColor,
         actions: [
           Container(
@@ -30,37 +33,40 @@ class _ContestListScreenState extends State<ContestListScreen> {
                   : Colors.white.withOpacity(0.25),
               borderRadius: BorderRadius.circular(15),
             ),
-            padding: EdgeInsets.only(right: 15),
-            child: DropdownButton<String>(
-              // style: TextStyle(backgroundColor: Colors.white),
-              // focusColor: Colors.white,
-              // iconEnabledColor: Colors.white,
-              // dropdownColor: Colors.white,
-              // iconDisabledColor: Colors.white,
-              value: dropDownValue,
-              icon: Icon(Icons.filter_alt),
-              items: itemList.map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 18.0),
-                    child: Text(
-                      value,
-                      style: TextStyle(
+            padding: const EdgeInsets.only(right: 15),
+            child: DropdownButtonHideUnderline(
+              child: DropdownButton<String>(
+                // style: TextStyle(backgroundColor: Colors.white),
+                // focusColor: Colors.white,
+                // iconEnabledColor: Colors.white,
+                // dropdownColor: Colors.white,
+                // iconDisabledColor: Colors.white,
+                value: dropDownValue,
+                icon: const Icon(Icons.filter_alt),
+                items: itemList.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 18.0),
+                      child: Text(
+                        value,
+                        style: TextStyle(
                           fontSize: 18,
-                          color: Theme.of(context).backgroundColor),
+                          color: Theme.of(context).backgroundColor,
+                        ),
+                      ),
                     ),
-                  ),
-                );
-              }).toList(),
-              onChanged: (String? value) {
-                setState(() {
-                  dropDownValue = value!;
-                  // print(dropDownValue);
-                  // currentChildId = getChildId(value);
-                  // StorageHandler().setChildId(currentChildId);
-                });
-              },
+                  );
+                }).toList(),
+                onChanged: (String? value) {
+                  setState(() {
+                    dropDownValue = value!;
+                    // print(dropDownValue);
+                    // currentChildId = getChildId(value);
+                    // StorageHandler().setChildId(currentChildId);
+                  });
+                },
+              ),
             ),
           )
         ],
@@ -68,7 +74,7 @@ class _ContestListScreenState extends State<ContestListScreen> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Container(
+          child: SizedBox(
             width: double.infinity,
             height: double.infinity,
             child: FutureBuilder(
@@ -78,7 +84,29 @@ class _ContestListScreenState extends State<ContestListScreen> {
                 if (snapshot.hasData) {
                   List<ContestModel> allContests = snapshot.data;
                   List<ContestModel> selectedList = [];
-                  if (dropDownValue == "All") {
+                  if (dropDownValue == "Today") {
+                    selectedList = allContests
+                        .where((element) => DateTime.fromMillisecondsSinceEpoch(
+                                element.startTimeSeconds * 1000)
+                            .isAfter(DateTime.now()))
+                        .toList();
+                    selectedList = selectedList
+                        .where((element) => DateTime.fromMillisecondsSinceEpoch(
+                                element.startTimeSeconds * 1000)
+                            .isBefore(DateTime.now().add(Duration(days: 1))))
+                        .toList();
+                  } else if (dropDownValue == "This Week") {
+                    selectedList = allContests
+                        .where((element) => DateTime.fromMillisecondsSinceEpoch(
+                                element.startTimeSeconds * 1000)
+                            .isAfter(DateTime.now()))
+                        .toList();
+                    selectedList = selectedList
+                        .where((element) => DateTime.fromMillisecondsSinceEpoch(
+                                element.startTimeSeconds * 1000)
+                            .isBefore(DateTime.now().add(Duration(days: 7))))
+                        .toList();
+                  } else if (dropDownValue == "All") {
                     selectedList = allContests;
                   } else if (dropDownValue == "Upcoming") {
                     selectedList = allContests
